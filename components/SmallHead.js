d0dc197e82725eb13ea5817e75fe7e764ed4c736
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-06-22 16:47:11 +0100

    suprememods

--- components/SmallHead.js
+++ components/SmallHead.js
@@ -9,15 +9,15 @@ export default function SmallHead() {
         <div className="w-screen flex justify-between items-center px-2 py-4">
             <div className="text-2xl text-white font-semibold">Priscilla <span className="text-amber-500">Luzes</span></div>
             <button className="p-2" onClick={toggleMobileMenu}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
             </button>
 
             <div className="bg-zinc-800 w-screen absolute inset-y-0 left-0 transform -translate-x-full transition duration-200 ease-in-out side-bar">
                 <button className="focus:outline-none focus:bg-zinc-700 p-4 mobile-menu-btn" onClick={toggleMobileMenu}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
                 <div className="text-white text-2xl h-full w-full grid place-items-center">
--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
     domains: ["i.ytimg.com"]
   }
