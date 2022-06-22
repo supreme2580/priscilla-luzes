d2b3023da238c9f638d105f724ce6d1a7387625f
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-06-22 16:29:36 +0100

    suprememods

--- components/Bio.js
+++ components/Bio.js
@@ -4,7 +4,7 @@ export default function Bio() {
             <div className="text-3xl sm:text-6xl text-amber-500 w-full text-center font-semibold">Bio<span className="text-white">graphy</span></div>
             <div className="w-full sm:flex justify-between space-x-4 space-y-4 mt-10">
                 <div className="w-full h-full text-white text-lg sm:text-2xl -mt-4 sm:mt-4">
-                    Priscilla Luzes is a 21 year old Brazilian-American actress,
+                     Priscilla Luzes is a 21 year old Brazilian-American actress,
                      born in Rio de Janeiro. She is studying for Media Arts and Theatre in the University 
                      of North Texas in Denton, TX. She is known for her role as Nadia, a superhero costume designer, 
                      in the YouTube web-series Mighty Moron (2019). She is recognized for her role in Arrows (2019), 
